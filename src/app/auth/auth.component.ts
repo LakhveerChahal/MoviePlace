79741e3ffa77b8b432ac313bb9aef250a3992f38
Lakhveer Singh <luckychahal4@gmail.com>
2020-06-20 05:29:30 -0700

    added sign up feature

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -8,10 +8,15 @@ import { AuthService } from '../services/auth.service';
   styleUrls: ['./auth.component.css']
 })
 export class AuthComponent implements OnInit {
+  isLoginMode = true;
 
   constructor(private authService: AuthService) { }
 
   ngOnInit(): void {
+  }
+
+  isLoggedIn(){
+    return this.authService.isLoggedIn();
   }
 
   onLogin(loginForm: NgForm){
@@ -21,6 +26,26 @@ export class AuthComponent implements OnInit {
       password: loginForm.value.password
     };
     console.log(this.authService.login(user));
+  }
+
+  onSubmit(form: NgForm){
+    if(!form.valid){
+      return;
+    }
+    const email = form.value.email;
+    const password = form.value.password;
+
+    if(this.isLoginMode){
+
+    }
+    else{
+      this.authService.signUp(email, password).subscribe((res) => {
+        console.log(res);
+      },
+      (err) => {
+        console.log(err);
+      });
+    }
   }
 
 }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { HttpClient} from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
@@ -7,9 +8,22 @@ export class AuthService {
   regUser = [{
     email: 'name@name.com', password: '12345678'
   }];
-  constructor() { }
+
+  constructor(private http: HttpClient) { }
 
   login(user: { email: string, password: string }): boolean {
     return this.regUser.includes(user);
+  }
+
+  isLoggedIn(){
+    return false;
+  }
+
+  signUp(email: string, password: string){
+    return this.http.post('https://identitytoolkit.googleapis.com/v1/accounts:signInWithCustomToken?key=AIzaSyC8ZMWtzxmH1xJj4xb7VULqZ0DGmAzG_j4', 
+    {
+      email: email,
+      password: password
+    });
   }
 }
